3baa6c8af2bf94d34d4169910ad8ec9b62f5a06b
Altiria <90601711+altiria@users.noreply.github.com>
2021-09-24 09:28:18 +0200

    Update TestAltiriaSmsJavaClientGetCreditAuth.java

--- src/test/java/com/altiria/app/TestAltiriaSmsJavaClientGetCreditAuth.java
+++ src/test/java/com/altiria/app/TestAltiriaSmsJavaClientGetCreditAuth.java
@@ -9,55 +9,53 @@ import org.junit.Test;
 
 import com.altiria.app.exception.JsonException;
 
-
 public class TestAltiriaSmsJavaClientGetCreditAuth {
 
-    //configurable parameters
-    public String login = "user@altiria.com";
-    public String password = "mypassword";
-    public boolean debug = false;
-    
-    private static final Logger log = LogManager.getLogger(TestAltiriaSmsJavaClientGetCreditAuth.class);
-    
-    /**
-     * The login parameter is missed.
-     */
-    @Test
-    public void testErrorNoLogin() {
-    	if (debug)
-    		log.debug("Enter testErrorNoLogin");
-    
-    	try {
-            AltiriaClient client = new AltiriaClient(null, password);
-            client.setDebug(debug);
-            client.getCredit();
-            fail("JsonException should have been thrown");
-    	} catch (JsonException je) {
-			assertEquals("LOGIN_NOT_NULL", je.getMessage());
-		} catch (Exception e) {
-			fail("Error: "+e.getMessage());
-		}
+  //configurable parameters
+  public String login = "user@altiria.com";
+  public String password = "mypassword";
+  public boolean debug = false;
+
+  private static final Logger log = LogManager.getLogger(TestAltiriaSmsJavaClientGetCreditAuth.class);
+
+  /**
+   * The login parameter is missed.
+   */
+  @Test
+  public void testErrorNoLogin() {
+    if (debug)
+      log.debug("Enter testErrorNoLogin");
+
+    try {
+      AltiriaClient client = new AltiriaClient(null, password);
+      client.setDebug(debug);
+      client.getCredit();
+      fail("JsonException should have been thrown");
+    } catch (JsonException je) {
+      assertEquals("LOGIN_NOT_NULL", je.getMessage());
+    } catch (Exception e) {
+      fail("Error: " + e.getMessage());
     }
-    
-    /**
-     * The password parameter is missed.
-     */
-    @Test
-    public void testErrorNoPassword() {
-    	if (debug)
-    		log.debug("Enter testErrorNoPassword");
-    
-    	try {
-            AltiriaClient client = new AltiriaClient(login, null);
-            client.setDebug(debug);
-            client.getCredit();
-            fail("JsonException should have been thrown");
-    	} catch (JsonException je) {
-			assertEquals("PASSWORD_NOT_NULL", je.getMessage());
-		} catch (Exception e) {
-			fail("Error: "+e.getMessage());
-		}
+  }
+
+  /**
+   * The password parameter is missed.
+   */
+  @Test
+  public void testErrorNoPassword() {
+    if (debug)
+      log.debug("Enter testErrorNoPassword");
+
+    try {
+      AltiriaClient client = new AltiriaClient(login, null);
+      client.setDebug(debug);
+      client.getCredit();
+      fail("JsonException should have been thrown");
+    } catch (JsonException je) {
+      assertEquals("PASSWORD_NOT_NULL", je.getMessage());
+    } catch (Exception e) {
+      fail("Error: " + e.getMessage());
     }
-    
-    
+  }
+
 }
