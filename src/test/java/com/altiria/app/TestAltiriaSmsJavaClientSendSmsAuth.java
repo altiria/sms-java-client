ebdf5f497a1468b4804317d483ce39cad7b212e6
Altiria <90601711+altiria@users.noreply.github.com>
2021-09-24 09:29:11 +0200

    Update TestAltiriaSmsJavaClientSendSmsAuth.java

--- src/test/java/com/altiria/app/TestAltiriaSmsJavaClientSendSmsAuth.java
+++ src/test/java/com/altiria/app/TestAltiriaSmsJavaClientSendSmsAuth.java
@@ -10,109 +10,108 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-
 public class TestAltiriaSmsJavaClientSendSmsAuth {
 
-    //configurable parameters
-    public String login = "user@altiria.com";
-    public String password = "mypassword";
-    //set to null if there is no sender
-    public String sender = "mySender";
-    public boolean debug = false;
-    public String destination = "346XXXXXXXX";
-    
-    private static final Logger log = LogManager.getLogger(TestAltiriaSmsJavaClientSendSmsAuth.class);
-    
-    /**
-     * The login parameter is missed.
-     */
-    @Test
-    public void testErrorNoLogin() {
-    	if(debug)
-    		log.debug("Enter testErrorNoLogin");
-    	
-    	try {
-    		String message = "Lorem Ipsum is simply dummy text";
-            
-            AltiriaClient client = new AltiriaClient(null, password);
-            client.setDebug(debug);
-            client.sendSms(destination, message);
-            fail("JsonException should have been thrown");
+  //configurable parameters
+  public String login = "user@altiria.com";
+  public String password = "mypassword";
+  //set to null if there is no sender
+  public String sender = "mySender";
+  public boolean debug = false;
+  public String destination = "346XXXXXXXX";
 
-    	} catch (JsonException je) {
-			assertEquals("LOGIN_NOT_NULL", je.getMessage());
-		} catch (Exception e) {
-			fail("Error: "+e.getMessage());
-		}
-    }
-    
-    /**
-     * The password parameter is missed.
-     */
-    @Test
-    public void testErrorNoPassword() {
-    	if(debug)
-    		log.debug("Enter testErrorNoPassword");
-    	
-    	try {
-    		String message = "Lorem Ipsum is simply dummy text";
-            
-            AltiriaClient client = new AltiriaClient(login, null);
-            client.setDebug(debug);
-            client.sendSms(destination, message);
-            fail("JsonException should have been thrown");
+  private static final Logger log = LogManager.getLogger(TestAltiriaSmsJavaClientSendSmsAuth.class);
 
-    	} catch (JsonException je) {
-			assertEquals("PASSWORD_NOT_NULL", je.getMessage());
-		} catch (Exception e) {
-			fail("Error: "+e.getMessage());
-		}
-    }
-    
-    /**
-     * The destination parameter is missed.
-     */
-    @Test
-    public void testErrorNoDestination() {
-    	if(debug)
-    		log.debug("Enter testErrorNoDestination");
-    	
-    	try {
-    		String message = "Lorem Ipsum is simply dummy text";
-            
-            AltiriaClient client = new AltiriaClient(login, null);
-            client.setDebug(debug);
-            client.sendSms(null, message);
-            fail("AltiriaGwException should have been thrown");
+  /**
+   * The login parameter is missed.
+   */
+  @Test
+  public void testErrorNoLogin() {
+    if (debug)
+      log.debug("Enter testErrorNoLogin");
 
-		} catch (AltiriaGwException ae) {
-			assertEquals("INVALID_DESTINATION", ae.getMessage());
-			assertEquals("015", ae.getStatus());
-		} catch (Exception e) {
-			fail("Error: "+e.getMessage());
-		}
-    }
-    
-    /**
-     * The message parameter is missed.
-     */
-    @Test
-    public void testErrorNoMessage() {
-    	if(debug)
-    		log.debug("Enter testErrorNoMessage");
-    	
-    	try {
-            AltiriaClient client = new AltiriaClient(login, null);
-            client.setDebug(debug);
-            client.sendSms(destination, null);
-            fail("AltiriaGwException should have been thrown");
+    try {
+      String message = "Lorem Ipsum is simply dummy text";
 
-		} catch (AltiriaGwException ae) {
-			assertEquals("EMPTY_MESSAGE", ae.getMessage());
-			assertEquals("017", ae.getStatus());
-		} catch (Exception e) {
-			fail("Error: "+e.getMessage());
-		}
+      AltiriaClient client = new AltiriaClient(null, password);
+      client.setDebug(debug);
+      client.sendSms(destination, message);
+      fail("JsonException should have been thrown");
+
+    } catch (JsonException je) {
+      assertEquals("LOGIN_NOT_NULL", je.getMessage());
+    } catch (Exception e) {
+      fail("Error: " + e.getMessage());
     }
-    
+  }
+
+  /**
+   * The password parameter is missed.
+   */
+  @Test
+  public void testErrorNoPassword() {
+    if (debug)
+      log.debug("Enter testErrorNoPassword");
+
+    try {
+      String message = "Lorem Ipsum is simply dummy text";
+
+      AltiriaClient client = new AltiriaClient(login, null);
+      client.setDebug(debug);
+      client.sendSms(destination, message);
+      fail("JsonException should have been thrown");
+
+    } catch (JsonException je) {
+      assertEquals("PASSWORD_NOT_NULL", je.getMessage());
+    } catch (Exception e) {
+      fail("Error: " + e.getMessage());
+    }
+  }
+
+  /**
+   * The destination parameter is missed.
+   */
+  @Test
+  public void testErrorNoDestination() {
+    if (debug)
+      log.debug("Enter testErrorNoDestination");
+
+    try {
+      String message = "Lorem Ipsum is simply dummy text";
+
+      AltiriaClient client = new AltiriaClient(login, null);
+      client.setDebug(debug);
+      client.sendSms(null, message);
+      fail("AltiriaGwException should have been thrown");
+
+    } catch (AltiriaGwException ae) {
+      assertEquals("INVALID_DESTINATION", ae.getMessage());
+      assertEquals("015", ae.getStatus());
+    } catch (Exception e) {
+      fail("Error: " + e.getMessage());
+    }
+  }
+
+  /**
+   * The message parameter is missed.
+   */
+  @Test
+  public void testErrorNoMessage() {
+    if (debug)
+      log.debug("Enter testErrorNoMessage");
+
+    try {
+      AltiriaClient client = new AltiriaClient(login, null);
+      client.setDebug(debug);
+      client.sendSms(destination, null);
+      fail("AltiriaGwException should have been thrown");
+
+    } catch (AltiriaGwException ae) {
+      assertEquals("EMPTY_MESSAGE", ae.getMessage());
+      assertEquals("017", ae.getStatus());
+    } catch (Exception e) {
+      fail("Error: " + e.getMessage());
+    }
+  }
+
 }
