231cfe31f7db146f2cc9e07ac0cc4961271d48a1
Altiria <90601711+altiria@users.noreply.github.com>
2021-09-24 09:26:13 +0200

    Update ConnectionException.java

--- src/main/java/com/altiria/app/exception/ConnectionException.java
+++ src/main/java/com/altiria/app/exception/ConnectionException.java
@@ -2,36 +2,36 @@ package com.altiria.app.exception;
 
 public class ConnectionException extends GeneralAltiriaException {
 
-	private String message;
+  private String message;
 
-	/**
-	 * Constructor.
-	 * @param message error message
-	 */
-	public ConnectionException(String message) {
-		super(message);
-		this.message = message;
-	}
+  /**
+   * Constructor.
+   * @param message error message
+   */
+  public ConnectionException(String message) {
+    super(message);
+    this.message = message;
+  }
 
-	/**
-	 * @return the message
-	 */
-	public String getMessage() {
-		return message;
-	}
+  /**
+   * @return the message
+   */
+  public String getMessage() {
+    return message;
+  }
 
-	/**
-	 * @param message the message to set
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
+  /**
+   * @param message the message to set
+   */
+  public void setMessage(String message) {
+    this.message = message;
+  }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "ConnectionException [message=" + message + "]";
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "ConnectionException [message=" + message + "]";
+  }
 }
