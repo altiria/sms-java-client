5eec313d17e0511cf7877791ee253179df311923
Altiria <90601711+altiria@users.noreply.github.com>
2021-09-24 09:26:58 +0200

    Update JsonException.java

--- src/main/java/com/altiria/app/exception/JsonException.java
+++ src/main/java/com/altiria/app/exception/JsonException.java
@@ -1,37 +1,37 @@
 package com.altiria.app.exception;
 
 public class JsonException extends GeneralAltiriaException {
-	
-	private String message;
 
-	/**
-	 * Constructor.
-	 * @param message error message
-	 */
-	public JsonException(String message) {
-		super(message);
-		this.message = message;
-	}
+  private String message;
 
-	/**
-	 * @return the message
-	 */
-	public String getMessage() {
-		return message;
-	}
+  /**
+   * Constructor.
+   * @param message error message
+   */
+  public JsonException(String message) {
+    super(message);
+    this.message = message;
+  }
 
-	/**
-	 * @param message the message to set
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
+  /**
+   * @return the message
+   */
+  public String getMessage() {
+    return message;
+  }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "JsonException [message=" + message + "]";
-	}
+  /**
+   * @param message the message to set
+   */
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "JsonException [message=" + message + "]";
+  }
 }
