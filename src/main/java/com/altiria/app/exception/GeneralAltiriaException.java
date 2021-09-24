038f7c00d0681744be19ffe80f6f4f3df109e10b
Altiria <90601711+altiria@users.noreply.github.com>
2021-09-24 09:26:36 +0200

    Update GeneralAltiriaException.java

--- src/main/java/com/altiria/app/exception/GeneralAltiriaException.java
+++ src/main/java/com/altiria/app/exception/GeneralAltiriaException.java
@@ -2,62 +2,62 @@ package com.altiria.app.exception;
 
 public class GeneralAltiriaException extends Exception {
 
-	private String message;
-	private String status;
-	
-	/**
-	 * Basic constructor.
-	 * @param message error message
-	 */
-	public GeneralAltiriaException(String message) {
-		super(message);
-		this.message = message;
-	}
-	
-	/**
-	 * Full constructor.
-	 * @param message error message
-	 * @param status status code
-	 */
-	public GeneralAltiriaException(String message, String status) {
-		super(message);
-		this.message = message;
-		this.status = status;
-	}
+  private String message;
+  private String status;
 
-	/**
-	 * @return the message
-	 */
-	public String getMessage() {
-		return message;
-	}
+  /**
+   * Basic constructor.
+   * @param message error message
+   */
+  public GeneralAltiriaException(String message) {
+    super(message);
+    this.message = message;
+  }
 
-	/**
-	 * @param message the message to set
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
+  /**
+   * Full constructor.
+   * @param message error message
+   * @param status status code
+   */
+  public GeneralAltiriaException(String message, String status) {
+    super(message);
+    this.message = message;
+    this.status = status;
+  }
 
-	/**
-	 * @return the status
-	 */
-	public String getStatus() {
-		return status;
-	}
+  /**
+   * @return the message
+   */
+  public String getMessage() {
+    return message;
+  }
 
-	/**
-	 * @param status the status to set
-	 */
-	public void setStatus(String status) {
-		this.status = status;
-	}
+  /**
+   * @param message the message to set
+   */
+  public void setMessage(String message) {
+    this.message = message;
+  }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "GeneralAltiriaException [message=" + message + ", status=" + status + "]";
-	}
+  /**
+   * @return the status
+   */
+  public String getStatus() {
+    return status;
+  }
+
+  /**
+   * @param status the status to set
+   */
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "GeneralAltiriaException [message=" + message + ", status=" + status + "]";
+  }
 }
